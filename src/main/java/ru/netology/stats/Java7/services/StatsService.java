412e3511fdbab11fa8517dd7bb3fe91ce3dc2098
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-02-14 22:30:28 +0700

    long2

--- src/main/java/ru/netology/stats/Java7/services/StatsService.java
+++ src/main/java/ru/netology/stats/Java7/services/StatsService.java
@@ -2,19 +2,19 @@ package ru.netology.stats.Java7.services;
 
 //номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму
 public class StatsService {
-    public int minSales(long[] sales) {
+    public long minSales(long[] sales) {
         long minMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] <= sales[(int) minMonth]) {
+            if (sales[i] <= sales [(int) minMonth]); {
                 minMonth = i;
             }
         }
-        return (int) (minMonth + 1);
+        return minMonth + 1;
     }
 
     // номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму
-    public int maxSales(long[] sales) {
+    public long maxSales(long[] sales) {
         long maxMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
@@ -22,31 +22,31 @@ public class StatsService {
                 maxMonth = i;
             }
         }
-        return (int) maxMonth + 1;
+        return maxMonth + 1;
     }
 
     //сумма всех продаж
-    public int sumSales(long[] sales) {
+    public long sumSales(long[] sales) {
         long sum = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            sum += (int) sales[i];
+            sum += sales[i];
         }
-        return (int) sum;
+        return sum;
     }
 
     //средняя сумма продаж в месяц
-    public int averageSales(long[] sales) {
+    public long averageSales(long[] sales) {
         long sum = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            sum += (int) sales[i];
+            sum += sales[i];
         }
-        return (int) (sum / sales.length);
+        return sum / sales.length;
     }
 
     //количество месяцев, в которых продажи были выше среднего
-    public int moreAverageSales(long[] sales) {
+    public long moreAverageSales(long[] sales) {
         long averageSales = averageSales(sales);
         long moreSales = 0;
 
@@ -55,11 +55,11 @@ public class StatsService {
                 moreSales ++;
             }
         }
-        return (int) moreSales;
+        return moreSales;
     }
 
     //количество месяцев, в которых продажи были ниже среднего
-    public int lessAverageSales(long[] sales) {
+    public long lessAverageSales(long[] sales) {
         long averageSales = averageSales(sales);
         long lessSales = 0;
 
@@ -68,6 +68,6 @@ public class StatsService {
                 lessSales ++;
             }
         }
-        return (int) lessSales;
+        return lessSales;
     }
 }
